2bdc497cc2e9069939e87b90d0965c99eda48d08
genzi560m1n9run-droid <genz.i5.60m1n9.run@gmail.com>
2026-05-05 21:38:37 -0300

    Camera fixed , more stuff missing .

---
+++ Assets/Scripts/ObstacleSetback.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ObstacleSetback : MonoBehaviour
+{
+    PlayerGoverner PlayerMovin;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
